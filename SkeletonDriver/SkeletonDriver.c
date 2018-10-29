2806bf38b54fc3bf70df02000b108d89f0e2cbdb
Francisco Charte <fcharte@ujaen.es>
2018-10-29 10:44:12 +0100

    Add carriage return to flush the message

--- SkeletonDriver/SkeletonDriver.c
+++ SkeletonDriver/SkeletonDriver.c
@@ -2,12 +2,12 @@
 #include<linux/kernel.h>
 
 static int __init init_driver(void) {
-    printk(KERN_INFO "Skeleton driver loaded");
+    printk(KERN_INFO "Skeleton driver loaded\n");
     return 0;
 }
 
 static void __exit exit_driver(void) {
-    printk(KERN_INFO "Skeleton driver unloaded");
+    printk(KERN_INFO "Skeleton driver unloaded\n");
 }
 
 MODULE_LICENSE("GPL");
