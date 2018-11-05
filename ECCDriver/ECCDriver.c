08c9f6c2456fbce89be662545240592d681543f8
Francisco Charte <fcharte@ujaen.es>
2018-11-05 12:24:08 +0100

    Rename variable holding major/minor numbers

--- ECCDriver/ECCDriver.c
+++ ECCDriver/ECCDriver.c
@@ -7,7 +7,7 @@
 #define DRIVER_CLASS	"ECCDriverClass"
 #define NUM_DEVICES	3
 
-static dev_t major_number = -1;
+static dev_t major_minor = -1;
 static struct cdev ECCcdev[NUM_DEVICES];
 static struct class *ECCclass = NULL;
 
@@ -29,12 +29,12 @@ static int __init init_driver(void) {
     int n_device;
     dev_t id_device;
 
-    if (alloc_chrdev_region(&major_number, 0, NUM_DEVICES, DRIVER_NAME) < 0)
+    if (alloc_chrdev_region(&major_minor, 0, NUM_DEVICES, DRIVER_NAME) < 0)
        goto error1;
 
     /* En este momento el controlador tiene asignado un "major number"
        Podemos consultarlo mirando en /proc/devices */
-    pr_info("%s driver assigned %d major number\n", DRIVER_NAME, MAJOR(major_number));
+    pr_info("%s driver assigned %d major number\n", DRIVER_NAME, MAJOR(major_minor));
 
     if((ECCclass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL)
        goto error2;
@@ -45,7 +45,7 @@ static int __init init_driver(void) {
     for (n_device = 0; n_device < NUM_DEVICES; n_device++) {
        cdev_init(&ECCcdev[n_device], &ECC_fops);
 
-       id_device = MKDEV(MAJOR(major_number), MINOR(major_number) + n_device);
+       id_device = MKDEV(MAJOR(major_minor), MINOR(major_minor) + n_device);
        if(cdev_add(&ECCcdev[n_device], id_device, 1) == -1)
           goto error3;
 
@@ -66,13 +66,13 @@ error1:
 
 error2: 
     pr_err("Class device registering failed");
-    unregister_chrdev_region(major_number, 1);
+    unregister_chrdev_region(major_minor, 1);
     return -2;
 
 error3:
     pr_err("Device node creation failed");
     class_destroy(ECCclass);
-    unregister_chrdev_region(major_number, 1);
+    unregister_chrdev_region(major_minor, 1);
     return -3;
 }
 
@@ -80,15 +80,15 @@ static void __exit exit_driver(void) {
     int n_device;
 
     for (n_device = 0; n_device < NUM_DEVICES; n_device++) {
-       device_destroy(ECCclass, MKDEV(MAJOR(major_number), MINOR(major_number) + n_device));
+       device_destroy(ECCclass, MKDEV(MAJOR(major_minor), MINOR(major_minor) + n_device));
        cdev_del(&ECCcdev[n_device]);
     }
     
     if(ECCclass) 
        class_destroy(ECCclass);
 
-    if(major_number != -1)
-       unregister_chrdev_region(major_number, 1);
+    if(major_minor != -1)
+       unregister_chrdev_region(major_minor, 1);
 
     pr_info("ECC driver unloaded\n");
 }
