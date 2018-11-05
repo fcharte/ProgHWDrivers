7ad527e552a6992710bff6a5f90c48b2049ab95a
Francisco Charte <fcharte@ujaen.es>
2018-11-05 10:38:38 +0100

    Register/Unregister major number and device class

--- ECCDriver/ECCDriver.c
+++ ECCDriver/ECCDriver.c
@@ -1,13 +1,45 @@
-#include<linux/module.h>
-#include<linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+#define DRIVER_NAME	"ECCDriver"
+#define DRIVER_CLASS	"ECCDriverClass"
+
+static dev_t major_number = -1;
+static struct cdev ECCcdev;
+static struct class *ECCclass = NULL;
 
 static int __init init_driver(void) {
-    printk(KERN_INFO "ECC driver initialized and loaded\n");
+    if (alloc_chrdev_region(&major_number, 0, 1, DRIVER_NAME) < 0)
+       goto error1;
+
+    // En este momento el controlador tiene asignado un "major number"
+    // Podemos consultarlo mirando en /proc/devices
+
+    if((ECCclass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL)
+       goto error2;
+
+    pr_info("ECC driver initialized and loaded\n");
     return 0;
+
+error1:
+    pr_err("Major number assignment failed");
+    return -1;
+
+error2: 
+    pr_err("Class device registering failed");
+    return -2;
 }
 
 static void __exit exit_driver(void) {
-    printk(KERN_INFO "ECC driver unloaded\n");
+    if(ECCclass) 
+       class_destroy(ECCclass);
+
+    if(major_number != -1)
+       unregister_chrdev_region(major_number, 1);
+
+    pr_info("ECC driver unloaded\n");
 }
 
 MODULE_LICENSE("GPL");
