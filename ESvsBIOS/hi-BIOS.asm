12cfdbce2c1f5ce8ef51f41c23ed7a0a1de4986f
Francisco Charte <francisco@fcharte.com>
2019-01-14 17:54:43 +0100

    Get ride of key release events

--- ESvsBIOS/hi-BIOS.asm
+++ ESvsBIOS/hi-BIOS.asm
@@ -1,13 +1,13 @@
   ORG 100h
 
 loop:
-  MOV AH,0
+  MOV AH,0   ; Get keypress
   INT 16h
  
-  CMP AL, 27
+  CMP AL, 27 ; Is it ESC key?
   JZ exit
 
-  MOV AH,0ah
+  MOV AH,0ah ; Show
   MOV CX,1
   INT 10h
 
--- ESvsBIOS/hi-INOUT.asm
+++ ESvsBIOS/hi-INOUT.asm
@@ -1,21 +1,31 @@
   ORG 100h
 
 loop:
-  IN AL,60h
- 
-  CMP AL,1
-  JZ exit
+  MOV AL, 0ABh
+  IN AL,64h   
+  TEST AL,1  ; The bit is never set in DOSBOX
+  ;JZ loop
 
-  MOV AH,0ah
-  MOV CX,1
-  INT 10h
+  IN AL,60h  ; Read port A
+  MOV AH, AL
 
-  IN AL, 61h
+  IN AL, 61h  ; ACK
   OR AL, 80h
   OUT 61h, AL
   AND AL, 7Fh
   OUT 61h, AL
 
+  CMP AH,1  ; Is it ESC key?
+  JZ exit
+
+  TEST AH,80h ; Is it a release event?
+  JNZ loop
+
+  MOV AL, AH  ; Show
+  MOV AH,0ah
+  MOV CX,1
+  INT 10h
+  
   JMP loop
 
 exit:
